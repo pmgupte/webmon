f35b1cba4d0b68e5ce67fc0af041d33d2a3eb8e9
Prabhas Gupte <pgupte@soleranetworks.com>
2013-12-09 20:11:34 +0530

    Added debug constants. Also replaced echo statements by calls to debug().

--- index.php
+++ index.php
@@ -11,10 +11,16 @@ if (!function_exists('curl_init')) {
  */
 define('FILE_SEEDS', './seeds');
 define('FILE_DATA_JSON', './data.json');
+define('FILE_DEBUG_LOG', './webmon.log');
 define('STATUS_NEW', 'New');
 define('STATUS_NO_CHANGE', 'No Change');
 define('STATUS_CHANGED', 'Changed');
+define('DEBUG_LEVEL_INFO', 'INFO');
+define('DEBUG_LEVEL_WARNING', 'WARNING');
+define('DEBUG_LEVEL_ERROR', 'ERROR');
 
+//TODO: set this flag according to -v flag on command line.
+define('VERBOSE', true);
 /*
  * Touch required files
  */
@@ -23,7 +29,7 @@ touch(FILE_DATA_JSON);
 
 $seeds = file(FILE_SEEDS, FILE_SKIP_EMPTY_LINES && FILE_IGNORE_NEW_LINES);
 $seedsCount = count($seeds);
-echo "\n$seedsCount URLs found in seeds file.";
+debug("$seedsCount URLs found in seeds file.");
 
 if (0 === count($seeds)) {
 	die('Exiting.');
@@ -37,7 +43,7 @@ $htmlDom = new DOMDocument;
 libxml_use_internal_errors(true);
 
 foreach ($seeds as $seed) {
-	echo "\n* Fetching: $seed";
+	debug("* Fetching: $seed");
 	curl_setopt($ch, CURLOPT_URL, $seed);
 	curl_setopt($ch, CURLOPT_REFERER, 'Webmon Script');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -54,21 +60,21 @@ foreach ($seeds as $seed) {
 		if (isset($data[$seed])) {
 			// we have processed this seed at least once before
 			if ($newChecksum !== $data[$seed]['checksum']) {
-				echo "...", STATUS_CHANGED;
+				debug("...", STATUS_CHANGED);
 				// web page changed. update stored data
 				$data[$seed]['status'] = STATUS_CHANGED;
 				$data[$seed]['checksum'] = $newChecksum;
 				$data[$seed]['contents'] = base64_encode($body);
 			} else {
 				// no change. just update status
-				echo "...", STATUS_NO_CHANGE;
+				debug("...", STATUS_NO_CHANGE);
 				$data[$seed]['status'] = STATUS_NO_CHANGE;
 			}
 
 			$data[$seed]['lastChecked'] = microtime();
 		} else {
 			// this is first processing of this seed
-			echo "...", STATUS_NEW;
+			debug("...", STATUS_NEW);
 			$data[$seed] = array(
 				'status' => STATUS_NEW,
 				'checksum' => $newChecksum,
@@ -89,14 +95,21 @@ echo "\n*** Done ***\n";
 /******************************************************************************
  * Helper Functions
  *****************************************************************************/
-function debug($level=DEBUG, $message) {
+function debug($message, $level=DEBUG_LEVEL_INFO) {
 	$timestamp = date('Y-m-d H:i:s');
 
 	if (is_string($message)) {
-		file_put_contents(LOGFILE, "[$timestamp] $level: $message", FILE_APPEND);
+		file_put_contents(FILE_DEBUG_LOG, "[$timestamp] $level: $message", FILE_APPEND);
+		if (VERBOSE) {
+			echo "\n", "[$timestamp] $level: $message";
+		}
 	} else {
-		file_put_contents(LOGFILE, "[$timestamp] $level: ", FILE_APPEND);
-		file_put_contents(LOGFILE, $message, FILE_APPEND);
+		file_put_contents(FILE_DEBUG_LOG, "[$timestamp] $level: ", FILE_APPEND);
+		file_put_contents(FILE_DEBUG_LOG, $message, FILE_APPEND);
+		if (VERBOSE) {
+			echo "\n", "[$timestamp] $level: ";
+			echo var_export($message, true);
+		}
 	}
-} // log
+} // debug
 ?>
