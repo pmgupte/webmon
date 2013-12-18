bf96929dca1a20cd3f0ebec6e312a9e96fe4104a
Prabhas Gupte <prabhas.gupte@gmail.com>
2013-12-18 18:18:42 +0530

    moved constants into constants.php, included this script into index.php

--- index.php
+++ index.php
@@ -6,21 +6,8 @@ if (!function_exists('curl_init')) {
 	die('Need PHP cURL installed and enabled.');
 }
 
-/*
- * Constants required for this script
- */
-define('FILE_SEEDS', './seeds');
-define('FILE_DATA_JSON', './data.json');
-define('FILE_DEBUG_LOG', './webmon.log');
-define('STATUS_NEW', 'New');
-define('STATUS_NO_CHANGE', 'No Change');
-define('STATUS_CHANGED', 'Changed');
-define('DEBUG_LEVEL_INFO', 'INFO');
-define('DEBUG_LEVEL_WARNING', 'WARNING');
-define('DEBUG_LEVEL_ERROR', 'ERROR');
+require('constants.php');
 
-//TODO: set this flag according to -v flag on command line.
-define('VERBOSE', true);
 /*
  * Touch required files
  */
