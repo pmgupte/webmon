6958acb99a417ad9c656b7b6901eb98e2a344d25
Prabhas Gupte <prabhas.gupte@gmail.com>
2014-01-05 15:11:03 +0530

    Removed FILE_SEEDS constant. This constant will get defined based on user input for -i/--inputfile option. Defaults to ./seeds

--- constants.php
+++ constants.php
@@ -2,7 +2,6 @@
 /*
  * Constants required for webmon/index.php script
  */
-define('FILE_SEEDS', './seeds');
 define('FILE_DATA_JSON', './data.json');
 define('FILE_DEBUG_LOG', './webmon.log');
 define('FILE_A_SUFFIX', '_a.txt');
@@ -13,8 +12,4 @@ define('STATUS_CHANGED', 'Changed');
 define('DEBUG_LEVEL_INFO', 'INFO');
 define('DEBUG_LEVEL_WARNING', 'WARNING');
 define('DEBUG_LEVEL_ERROR', 'ERROR');
-
-//TODO: set this flag according to -v flag on command line.
-define('VERBOSE', true);
-
 ?>
