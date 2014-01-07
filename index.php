999162445714e943c76caee2bf8665bb16065083
Prabhas Gupte <prabhas.gupte@gmail.com>
2014-01-07 13:58:27 +0530

    Added GNU GPL license notice to source files.

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 #!/usr/bin/php
 <?php
+/**
+* Webmon - program to monitor web pages for change and detect the change
+* Copyright (C) 2013 Prabhas Gupte
+* 
+* This file is part of Webmon.
+* 
+* Webmon is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Webmon is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Webmon.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 /*
  * Primary Checks
  */
