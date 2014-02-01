b85586e421a80a50c1a881228511b84df7e42403
Prabhas Gupte <prabhas.gupte@gmail.com>
2014-02-01 09:45:15 +0530

    unit test for command without -i option

--- test/WebmonTest.php
+++ test/WebmonTest.php
@@ -13,5 +13,13 @@ class WebmonTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertTrue($optionsFound);
 	}
+
+	public function testNoInputOption() {
+		$output = array();
+		exec("php {$this->testDir}/webmon.php -s -t60", $output, $returnValue);
+		$messageFound = in_array('Please use either --inputfile or --url option. One of them is required.', $output);
+
+		$this->assertTrue($messageFound);
+	}
 }
 ?>
